c847fd3ee7cf3ee484292b2cbae5bf225c3e8a19
armaanepiic <armaanepiicfb17@gmail.com>
2025-08-13 17:21:16 +0600

    Basic App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
-
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Sidebar from './components/Sidebar'
 
 export default function App() {
   return (
-    <div>Hello React</div>
+    <div>
+      <Header/>
+      <div className=''>
+        <Sidebar/>
+        <MainContent/>
+      </div>
+    </div>
   )
 }
