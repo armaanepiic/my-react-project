c955c5b7be14afa9366bad30f271363e51b0686f
armaanepiic <armaanepiicfb17@gmail.com>
2025-08-13 22:59:20 +0600

    context api done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import { useState } from "react";
 
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
 
 import "./App.css";
+import ThemeProvider from "./providers/ThemeProvider";
 
 export default function App() {
-  const [theme, setTheme] = useState("light");
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  };
-
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header theme={theme} toggleTheme={toggleTheme} />
-      <div className="flex flex-1">
-        <Sidebar theme={theme} />
-        <MainContent />
+    <ThemeProvider>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <div className="flex flex-1">
+          <Sidebar />
+          <MainContent />
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
