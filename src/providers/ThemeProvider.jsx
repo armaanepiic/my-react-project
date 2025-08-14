d171f8ef5db24e5ce5505edf1717b4d97219b948
armaanepiic <armaanepiicfb17@gmail.com>
2025-08-14 15:38:40 +0600

    include search params

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,12 +1,20 @@
 // Theme provider
 // A wrapper component that manages the actual theme state using useState
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ThemeContext from "../contexts/ThemeContext";
+import { useSearchParams } from "react-router";
 
 // Contains the toggle logic to switch between "light" and "dark" themes
 const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
+  const [searchParamas, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setSearchParams({ ...searchParamas, mode: theme });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [theme]);
+  
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
