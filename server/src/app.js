95c318bf0c2937070472140ff143bbd618b7ef57
armaanepiic <armaanepiicfb17@gmail.com>
2025-08-18 12:06:13 +0600

    nodemon

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const { hello } = require("./test");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// const { hello } = require("./test");
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+    // hello(),
+    res.send("Hello World!");
+});
+app.get("/test", (req, res) => {
+    hello();
+    res.send("test");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
