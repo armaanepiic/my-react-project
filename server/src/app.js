eba2c95e313ff2b50fbc88301d3e62eacd0d4707
arman <armaanepiic@gmail.com>
2026-01-21 15:24:29 +0600

    -express js server setup, nodemon, script changed

---
+++ server/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express"); // loads a package (express) installed in node_modules
+const app = express();  // creates an Express application object. app is now your server “controller” where you define routes, middleware, settings, etc.
+const port = 5000; //This is the port number your server will listen on. http://localhost:5000
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
