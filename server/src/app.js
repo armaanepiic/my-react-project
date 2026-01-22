00d8b14cf63a6121ca18ecb4ae6f664c64a7c3b1
arman <armaanepiic@gmail.com>
2026-01-23 02:28:27 +0600

    env configuration done

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,16 @@
 import cors from "cors";
 import express from "express"; // loads a package (express) installed in node_modules
+import envConfig from "./config/envConfig.js";
 import { mockProducts } from "./mockdata.js";
 
 const app = express(); // creates an Express application object. app is now your server “controller” where you define routes, middleware, settings, etc.
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:3000",
-}));
 
-const port = 5000; //This is the port number your server will listen on. http://localhost:5000
+app.use(
+  cors({
+    origin: envConfig.ALLOWED_ORIGIN,
+  }),
+);
 
 app.get("/status", (req, res) => {
   res.json({
@@ -16,10 +18,10 @@ app.get("/status", (req, res) => {
   });
 });
 app.get("/api/products", (req, res) => {
-  // res.status(200).json(mockProducts);
-  res.send(mockProducts);
+  res.status(200).json(mockProducts);
+  // res.send(mockProducts);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(envConfig.PORT, () => {
+  console.log(`Example app listening on port ${envConfig.PORT}`);
 });
