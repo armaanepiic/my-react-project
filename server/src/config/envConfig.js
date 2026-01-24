d29d0631e3c36eb30f4e30eb2e3da306feaa024d
arman <armaanepiic@gmail.com>
2026-01-24 21:28:51 +0600

    dot env library installed

--- server/src/config/envConfig.js
+++ server/src/config/envConfig.js
@@ -1,6 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const envConfig = {
-  PORT: 5000,
-  ALLOWED_ORIGIN: "http://localhost:3000",
+  PORT: process.env.PORT,
+  ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN,
 };
 
 export default envConfig;
+
+// PORT: process.env.PORT,    => process.env.<same as .env>
+// ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN, => process.env.<same as .env>
